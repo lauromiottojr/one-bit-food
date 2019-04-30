7881b9086d10d629612c32846e62741f3610c36e
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-29 22:02:23 -0300

    Implementing Controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
-  def index
+  def index # renderiza todas as categorias
+    @categories = Category.all.order(:title)
+    render json: @categories
   end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,29 @@
 class RestaurantsController < ApplicationController
-  def index
+  before_action :set_restaurant, only: :show
+  def index # lista restaurantes proximos
+    @restaurants = Restaurant.near(params[:city] || 'São Paulo') # model.near procura os endereços proximos
+    # procura por cidade primeiro, se nao existir, procura por SP
+    filter_by_category if params[:category] # filtrar restaurantes por categoria, se o parametro categoria == true
+    render json: @restaurants # renderiaza os restaurantes
   end
 
   def show
+    render json: @restaurant, product_categories: true # quando chama o show url fica restaurants/?id
   end
 
   def search
   end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find_by(id: params[:id]) # procura o restaurante por id
+  end
+
+  def filter_by_category
+    @restaurants = @restaurants.select do |r| # selecionar os elementos de tal caracteristica
+      r.category.title == params[:category] # no caso, de tal categoria
+    end
+  end
+
 end
