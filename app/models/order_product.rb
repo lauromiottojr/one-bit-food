95dba221a5b1a2f21970077a55f841d41eb18df1
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-29 21:09:45 -0300

    Adding Validates and Enums

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,8 @@
 class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates_associated :order
+  validates_associated :product # required
+  validates :quantity, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates_associated :restaurant
+  validates :value, inclusion: 1..5 # value between 1 and 5
 end
